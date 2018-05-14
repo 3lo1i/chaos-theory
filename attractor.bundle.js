6fe785398a5c641447992aa1b8a8739abb2571cf
3lo1i <3lo1i@protonmail.com>
2018-05-14 21:52:41 +0300

    Updates

---
+++ attractor.bundle.js
@@ -0,0 +1,50 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["attractor"],{
+
+/***/ "./src/modules/attractor/attractor.js":
+/*!********************************************!*\
+  !*** ./src/modules/attractor/attractor.js ***!
+  \********************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _attractor_pug__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./attractor.pug */ "./src/modules/attractor/attractor.pug");
+/* harmony import */ var _attractor_pug__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_attractor_pug__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+console.log('attractor module has loaded');
+
+const init = () => {
+  console.log('attractor init');
+  return jquery__WEBPACK_IMPORTED_MODULE_1___default()(_attractor_pug__WEBPACK_IMPORTED_MODULE_0___default()());
+};
+
+const destroy = () => {
+  console.log('attractor destroyed');
+};
+
+/* harmony default export */ __webpack_exports__["default"] = ({ init, destroy });
+
+
+/***/ }),
+
+/***/ "./src/modules/attractor/attractor.pug":
+/*!*********************************************!*\
+  !*** ./src/modules/attractor/attractor.pug ***!
+  \*********************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var pug = __webpack_require__(/*! ../../../node_modules/pug-runtime/index.js */ "./node_modules/pug-runtime/index.js");
+
+function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;pug_html = pug_html + "\u003Ch2\u003EАттрактор Лоренца\u003C\u002Fh2\u003E\u003Cdiv\u003ETBD\u003C\u002Fdiv\u003E";;return pug_html;};
+module.exports = template;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=attractor.bundle.js.map
---
+++ lotka.bundle.js
@@ -0,0 +1,50 @@
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([["lotka"],{
+
+/***/ "./src/modules/lotka/lotka.js":
+/*!************************************!*\
+  !*** ./src/modules/lotka/lotka.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _lotka_pug__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./lotka.pug */ "./src/modules/lotka/lotka.pug");
+/* harmony import */ var _lotka_pug__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_lotka_pug__WEBPACK_IMPORTED_MODULE_0__);
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_1__);
+
+
+
+console.log('lotka module has loaded');
+
+const init = () => {
+  console.log('lotka init');
+  return jquery__WEBPACK_IMPORTED_MODULE_1___default()(_lotka_pug__WEBPACK_IMPORTED_MODULE_0___default()());
+};
+
+const destroy = () => {
+  console.log('lotka destroyed');
+};
+
+/* harmony default export */ __webpack_exports__["default"] = ({ init, destroy });
+
+
+/***/ }),
+
+/***/ "./src/modules/lotka/lotka.pug":
+/*!*************************************!*\
+  !*** ./src/modules/lotka/lotka.pug ***!
+  \*************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+var pug = __webpack_require__(/*! ../../../node_modules/pug-runtime/index.js */ "./node_modules/pug-runtime/index.js");
+
+function template(locals) {var pug_html = "", pug_mixins = {}, pug_interp;pug_html = pug_html + "\u003Ch2\u003EМодель Лотки — Вольтерры\u003C\u002Fh2\u003E\u003Cdiv\u003ETBD\u003C\u002Fdiv\u003E";;return pug_html;};
+module.exports = template;
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lotka.bundle.js.map
