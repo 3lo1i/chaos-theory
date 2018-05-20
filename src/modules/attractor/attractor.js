6c3f4e41df2fb64209790f501fa255ee5425cab9
Unknown <3lo1i@protonmail.com>
2018-05-20 19:04:02 +0300

    Add attractor activity

--- src/modules/attractor/attractor.js
+++ src/modules/attractor/attractor.js
@@ -5,9 +5,103 @@ console.log('attractor module has loaded');
 
 const title = 'Аттрактор Лоренца';
 
+const data = {
+  'a': 5,
+  'b': 15,
+  'c': 1,
+  'dt': 0.0001,
+  'iterations': 500000
+};
+
+const HSVtoRGB = (h) => {
+  const i = Math.floor(h * 6);
+  const f = Math.round((h * 6 - i) * 255);
+  const q = Math.round(255 - f);
+  switch (i % 6) {
+    case 0: return [255, f, 0]; break;
+    case 1: return [q, 255, 0]; break;
+    case 2: return [0, 255, f]; break;
+    case 3: return [0, q, 255]; break;
+    case 4: return [f, 0, 255]; break;
+    case 5: return [255, 0, q]; break;
+  }
+};
+
+
+const drawAttractor = (context, w, h, {a, b, c, dt, iterations}) => {
+  let [x, y, z] = [3.051522, 1.582542, 15.62388];
+  context.clearRect(0, 0, w, h);
+  var imageData = context.getImageData(0, 0, w, h);
+  var idx = 0;
+  const iterationPercent = 1 / iterations;
+  var i = iterations;
+  while (i--) {
+    [x, y, z] = [
+      x + a * (-x + y) * dt,
+      y + (b * x - y - z * x) * dt,
+      z + (-c * z + x * y) * dt
+    ];
+    idx = 4 * (Math.round(19.3 * (y - x * 0.292893) + 320) + Math.round(-11 * (z + x * 0.292893) + 392) * w);
+    const hue = i * iterationPercent;
+    const [col_r, col_g, col_b] = HSVtoRGB(hue);
+    imageData.data[idx + 0] = col_r;  // r
+    imageData.data[idx + 1] = col_g;  // g
+    imageData.data[idx + 2] = col_b;  // b
+    imageData.data[idx + 3] = 255;  // a
+  }
+  context.putImageData(imageData, 0, 0);
+};
+
+
 const init = () => {
   console.log('attractor init');
-  return $(template());
+
+  const activity = $(template());
+
+  const canvas = activity.find('#attractor-canvas');
+  const context = canvas[0].getContext('2d');
+  const h = parseInt(canvas.attr("height"));
+  const w = parseInt(canvas.attr("width"));
+
+  activity
+    .find('input[data-param]')
+    .on('input', (e) => {
+      const input = e.target;
+      const { param } = input.dataset;
+      if (param === 'iterations') {
+        data[param] = parseFloat(input.value) * 100;
+      } else {
+        data[param] = parseFloat(input.value);
+      }
+      console.log(data);
+      const t = Date.now();
+      drawAttractor(context, w, h, data);
+      console.log(Date.now() - t);
+    });
+
+  drawAttractor(context, w, h, data);
+
+  const saveAs = (dataUrl, filename) => {
+    const anchor = document.createElement('a');
+    anchor.setAttribute('href', dataUrl);
+    anchor.setAttribute('target', '_blank');
+    anchor.setAttribute('download', filename);
+    if (document.createEvent) {
+      const evtObj = document.createEvent('MouseEvents');
+      evtObj.initEvent('click', true, true);
+      anchor.dispatchEvent(evtObj);
+    }
+    else if (anchor.click) {
+      anchor.click();
+    }
+  };
+
+  activity.find('#save-png-btn').click((e) => {
+    saveAs(canvas[0].toDataURL(), 'attractor.png');
+    e.preventDefault();
+  });
+
+  return activity;
 };
 
 const destroy = () => {
